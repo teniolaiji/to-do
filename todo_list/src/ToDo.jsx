8a44a472d76b487d4bda515818d7cf0a909b8c00
t-iji7 <o.iji@alustudent.com>
2025-07-22 18:08:09 +0200

    handle input change

--- todo_list/src/ToDo.jsx
+++ todo_list/src/ToDo.jsx
@@ -5,6 +5,9 @@ function ToDoList() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
+  function handleChange(event) {
+    setNewTask(event.target.value);
+  }
   function addTask() {
     setTasks([...tasks, newTask]);
     setNewTask("");
