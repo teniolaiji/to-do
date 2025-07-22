c748404afcd809e090ff8ef9b186628c899fce10
t-iji7 <o.iji@alustudent.com>
2025-07-22 18:33:31 +0200

    functionality complete

--- todo_list/src/ToDo.jsx
+++ todo_list/src/ToDo.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 function ToDoList() {
   const [tasks, setTasks] = useState([]);
@@ -8,11 +7,47 @@ function ToDoList() {
   function handleChange(event) {
     setNewTask(event.target.value);
   }
-  function addTask() {
+  function addTask(event) {
     setTasks([...tasks, newTask]);
     setNewTask("");
   }
-
-  return;
+  function removeTask(index) {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  }
+  return (
+    <div className="text-center mt-6">
+      <h1 className="text-3xl font-bold mb-4 text-gray-500">todos</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Add todo..."
+          value={newTask}
+          onChange={handleChange}
+          className="p-2 border border-gray-300 rounded mr-2 "
+        />
+        <button
+          className="ml-2 bg-green-500 hover:bg-green -700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+          onClick={addTask}
+        >
+          Add
+        </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="mr-2 text-black font-semibold">{task}</span>
+            <button
+              className="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+              onClick={() => removeTask(index)}
+            >
+              X
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 export default ToDoList;
